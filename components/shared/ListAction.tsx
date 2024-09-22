cacb6ff45594af749cbfff0a04ef5a5e25119afe
AvikNayak <nayakavik19@gmail.com>
2024-09-22 13:08:54 +0530

    started working on list action component

---
+++ components/shared/ListAction.tsx
@@ -0,0 +1,12 @@
+import { IFolderAndFile } from "@/types";
+import React from "react";
+
+interface ListActionProps {
+  item: IFolderAndFile;
+}
+
+const ListAction = ({ item }: ListActionProps) => {
+  return <div>ListAction</div>;
+};
+
+export default ListAction;
